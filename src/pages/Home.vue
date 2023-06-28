2e2f0bb75d0ee2bad71ec068ec693543753cd8c7
Robuchon Hugues <robuchon.hugues@orange.fr>
2023-06-29 01:10:30 +0200

    content de la page Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,71 +1,103 @@
 <template>
-  <div>
-    <h1>Theme</h1>
-    <div name="fontTheme" class="my-5">
-      <h1>Balise texte</h1>
-      <div class="flex items-center gap-4">
-        <h1>Texte H1</h1>
-        <h2>Texte H2</h2>
-        <h3>Texte H3</h3>
-        <h4>Texte H4</h4>
-        <h5>Texte H5</h5>
-        <h6>Texte H6</h6>
-        <p>Texte P</p>
-        <a href="">Texte A</a>
-      </div>
+  <div class="flex max-w-2xl flex-col items-center">
+    <h1 class="text-center">Bonjour Chez moi</h1>
+    <h3 class="max-w-lg text-center">
+      Un accueil chaleureux... Parce que je préfère simplifier et vous guider
+      dans mon parcours.
+    </h3>
+    <h4 class="text-center">
+      Permettez-moi de vous partager mon parcours, qui a été une aventure riche
+      en expériences et en rebondissements.
+    </h4>
+    <div class="mt-8 space-y-4">
+      <p>
+        Après avoir terminé mes études en restauration, j'ai obtenu un diplôme
+        professionnel en 2005. Cependant, j'ai pris la décision de changer de
+        voie et de me lancer dans la pâtisserie en suivant une formation CAP.
+        Bien que je n'aie pas passé l'examen final, cette période a été marquée
+        par une circonstance importante : une blessure grave à la jambe qui m'a
+        contraint à une pause de plus de 10 mois.
+      </p>
+      <p>
+        Une fois rétabli, ma soif de découvrir de nouveaux horizons
+        professionnels m'a poussé à explorer différentes opportunités. J'ai
+        ainsi travaillé en tant qu'intérimaire pendant 4 ans et j'ai même eu
+        l'occasion de vivre deux saisons passionnantes avec des forains.
+        Toutefois, mon parcours ne s'est pas arrêté là.
+      </p>
+      <p>
+        En 2009, j'ai rejoint l'équipe de Micromania en tant que vendeur, et
+        j'ai eu l'opportunité de progresser dans mon parcours professionnel. Au
+        fil des années, j'ai gravi les échelons pour devenir responsable de
+        magasin, assumant ainsi des responsabilités croissantes au sein de cette
+        entreprise renommée. J'ai eu le privilège de travailler dans l'un des
+        plus grands magasins de France, ce qui m'a offert une expérience
+        inestimable.
+      </p>
+      <p>
+        Cette expérience chez Micromania, qui a duré plus de 12 ans jusqu'en
+        septembre 2021, a été une période marquante de ma carrière. Cependant,
+        au fur et à mesure de ma progression, j'ai ressenti le besoin
+        d'entreprendre une transition professionnelle significative pour
+        exploiter pleinement mon potentiel.
+      </p>
+      <p>
+        C'est ainsi que j'ai décidé de rejoindre la Bocal Academy pour suivre
+        leur formation FullStack JS, axée sur le stack MERN (MongoDB, Express,
+        React, Node). Malheureusement, quelques jours après la fin de cette
+        formation prometteuse, j'ai dû faire face à des problèmes de santé
+        majeurs.
+      </p>
+      <p>
+        Malgré ces défis, j'ai puisé ma force dans ma détermination et j'ai
+        choisi de transformer cette période de convalescence en une opportunité.
+        Je me suis lancé en tant que freelance, prêt à mettre à profit mes
+        compétences et mon expérience pour aider mes clients à atteindre leurs
+        objectifs.
+      </p>
+      <p>
+        Je suis impatient de partager mon expertise avec vous et de collaborer
+        pour concrétiser vos projets.
+      </p>
     </div>
-    <div name="colorTheme" class="my-5">
-      <h1>Colors</h1>
-      <div class="flex space-x-2">
-        <div>
-          <h5 class="text-Primary">Primary</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Primary">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-        <div>
-          <h5 class="text-Secondary">Secondary</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Secondary">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-        <div class="bg-Primary">
-          <h5 class="text-Accent">Accent</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Accent">
-            <p class="m-auto text-Primary">!!hello!!</p>
-          </div>
-        </div>
-        <div>
-          <h5 class="text-Neutral">Neutral</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Neutral">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-        <div>
-          <h5 class="text-Info">Info</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Info">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-        <div>
-          <h5 class="text-Success">Success</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Success">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-        <div>
-          <h5 class="text-Warning">Warning</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Warning">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-        <div>
-          <h5 class="text-Error">Error</h5>
-          <div class="flex h-8 w-14 rounded-full bg-Error">
-            <p class="m-auto text-Accent">!!hello!!</p>
-          </div>
-        </div>
-      </div>
+    <h4 class="mt-8 text-center text-Secondary">
+      Maintenant que les bases sont posées, passons au développement.
+    </h4>
+    <div class="mt-8 space-y-4">
+      <p>
+        J'ai toujours eu une passion pour l'informatique dans son ensemble, mais
+        j'ai longtemps cru que sans un diplôme universitaire et des études
+        spécialisées, je ne pourrais jamais réussir ou être pris au sérieux dans
+        ce domaine. Heureusement, en 2020, j'ai eu la chance de rencontrer des
+        personnes évoluant dans le milieu informatique qui m'ont encouragé. Je
+        me suis alors lancé dans l'apprentissage autonome du PHP, de l'HTML et
+        du CSS. À l'été 2021, j'ai publié mon premier site, qui n'est plus en
+        ligne aujourd'hui, car la personne pour qui je l'ai créé ne l'a jamais
+        utilisé. Vous pouvez trouver le lien du dépôt Git ici :
+        <a href="https://github.com/Robuchon/Le-petit-salon" target="_blank"
+          >Lien du dépôt Git.</a
+        >
+      </p>
+      <p>
+        En septembre, j'ai décidé de me lancer dans la formation proposée par le
+        Bocal Academy en tant que développeur web FullStack JS. Cela m'a
+        tellement enthousiasmé que j'ai quitté mon emploi début 2022 pour
+        devenir freelance. Depuis lors, j'ai travaillé sur différents projets,
+        notamment la création d'un site vitrine et d'une application web pour
+        <a href="https://reave.gg"  target="_blank">REAVE (reave.gg)</a> et d'autres projets privés. J'ai également eu
+        l'occasion de mettre à l'épreuve mes compétences sur des projets
+        semi-personnels, tels que le site d'art de ma femme, réalisé en moins de
+        10 heures :
+        <a href="https://ladyacadia.fr/home" target="_blank">Site d'art de ma femme.</a>
+      </p>
+      <p>
+        Et récemment, j'ai conçu un site vitrine pour l'entreprise de mon
+        beau-père. J'ai réussi à le réaliser en moins de 48 heures, en y
+        consacrant 9 heures de travail. Vous pouvez le découvrir ici :
+        <a href="https://9ca2847f.adp.pages.dev/home" target="_blank"
+          >Site vitrine de l'entreprise de mon beau-père."</a
+        >
+      </p>
     </div>
   </div>
 </template>
