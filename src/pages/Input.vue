459ce1d1d748bda3490c3ec568deaf8d84983276
Robuchon Hugues <robuchon.hugues@orange.fr>
2023-06-29 10:41:49 +0200

    amélioration de la page input

--- src/pages/Input.vue
+++ src/pages/Input.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="flex flex-col items-center w-full">
-    <h1>je vais faire un menue deroulant de fichier input</h1>
-    <div class="flex items-center">
+  <div class="flex w-full max-w-2xl flex-col items-center">
+    <h3>Menu déroulant de fichiers input</h3>
+<p>
+  Ici, je crée des petits bancs d'essai pour des inputs personnalisés. J'expérimente en mêlant mes connaissances pour pouvoir modifier facilement l'apparence d'un site en quelques clics. Mon objectif principal est de pouvoir créer rapidement des sites vitrines en quelques heures. Cette expérience m'a également permis de comprendre comment importer dynamiquement des composants et utiliser la directive ":is" pour la modification en temps réel d'une page.
+</p>
+
+    <div class="flex flex-col md:flex-row items-center">
       <InputsTarget
-      class=" w-80"
-      :data="selectInput"
-      :error="''"
-      v-model="testData.target"
-    ></InputsTarget>
-    <InputsTarget
-      class=" w-40"
-      :data="switchInput"
-      :error="''"
-      v-model="switchInput.value"
-    ></InputsTarget>
+        class="w-80"
+        :data="selectInput"
+        :error="''"
+        v-model="testData.target"
+      ></InputsTarget>
+      <InputsTarget
+        class="w-80"
+        :data="nameInput"
+        :error="''"
+        v-model="testData.label"
+      ></InputsTarget>
+      <InputsTarget
+        class="w-40"
+        :data="switchInput"
+        :error="''"
+        v-model="switchInput.value"
+      ></InputsTarget>
     </div>
-    <div class="flex items-center">
+    <div class="flex flex-col items-center">
       <InputsTarget
-      class=" w-80"
-      :data="testData"
-      :error="switchInput.value ? testError : ''"
-      v-model="testData.value"
-    ></InputsTarget>
-    <p>value {{ testData.value }}</p>
+        class="w-80"
+        :data="testData"
+        :error="switchInput.value ? testError : ''"
+        v-model="testData.value"
+      ></InputsTarget>
+      <p>value {{ testData.value }}</p>
     </div>
   </div>
 </template>
@@ -43,6 +53,13 @@ const selectInput = ref({
     { slot: "Input Text", value: "InputText" },
     { slot: "Input Switch", value: "InputSwitch" },
   ],
+});
+
+const nameInput = ref({
+  label: "Nom de l'input",
+  name: "nameInput",
+  type: "text",
+  target: "InputText",
 });
 
 const switchInput = ref({
